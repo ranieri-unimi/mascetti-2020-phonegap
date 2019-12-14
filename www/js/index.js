a562f78d4d9c4e5f660902ffae20104e62c3f588
ranieri-unimi <martinvincente.ranieri@studenti.unimi.it>
2019-12-14 16:36:45 +0100

    Basic layout implemented

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,50 @@
+var str = {
+    SESSION_ID : 'sDpnK5c9APmw4ERC',
+    BASE_URL : 'https://ewserver.di.unimi.it/mobicomp/mostri/URIRES.php',
+    REGISTER : 'register',
+    GET_MAP : 'getmap',
+    GET_IMAGE : 'getimage',
+    FIGHT_EAT : 'fighteat',
+    RANKING : 'ranking',
+    GET_PROFILE : 'getprofile',
+    SET_PROFILE : 'setprofile',
+    EMPTY_JSON : JSON.parse('{}'),
+    MAPBOX_TOKEN: 'pk.eyJ1IjoibWFmZmluOTkiLCJhIjoiY2szNGF0NnQ3MGs2YzNnbnk4dDVwaHd0YiJ9.XYdHDkZRn2iHY1xmHSMc1A',
+};
+
+var fnc = {
+
+    reqUrl : function (resource) {
+        return str.BASE_URL.replace('URIRES', resource);
+    },
+
+    sendRequest: function (url, data, success) {
+        data.session_id = str.SESSION_ID;
+        $.ajax({
+            method: 'post',
+            url:  fnc.reqUrl(url),
+            data: JSON.stringify(data),
+            type: 'POST',
+            dataType: 'json',
+            success: success,
+            error: function () { alert( 'Error network, check your Internet connection.' ); }
+        });
+    },
+
+    changeSection : function (id) {
+        $("section").hide();
+        $("#"+id).show();
+    },
+
+    loadMapBox : function () {
+        mapboxgl.accessToken = str.MAPBOX_TOKEN;
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11'
+        });
+    }
+};
+
 var app = {
 
     initialize: function() {
@@ -9,9 +56,11 @@ var app = {
     },
 
     onDeviceReady: function() {
+        document.addEventListener('pause',app.onPause, false);
+        document.addEventListener('resume',app.onResume, false);
+
+        // Here we go!
         app.receivedEvent('deviceready');
-        document.addEventListener("pause",app.onPause, false);
-        document.addEventListener("resume",app.onResume, false);
     },
 
     onPause: function() {
@@ -21,15 +70,13 @@ var app = {
     },
 
     receivedEvent: function(id) {
-        $("#btn").click(smaug.sendRequest(
-            'getmap',
-            JSON.parse('{}'),
-            app.onMapResult,
-            smaug.networkError
-        ));
+        //$("#btn").click(function(){ fnc.sendRequest(str.GET_MAP, str.EMPTY_JSON, app.onMapResult); });
+        $("#btn-lx").click(function() { fnc.changeSection('map'); fnc.loadMapBox(); });
+        $("#btn-lx").click();
+        $("#btn-rx").click(function() { fnc.changeSection('chart'); });
     },
 
     onMapResult : function (result) {
-        // Mappa pronta
+        console.log(result);
     },
 };
